16b3bf2e2a19ed298beedf52fb8cc37e751b1b64
ImanolSayago <imanolsayago0@gmail.com>
2024-05-24 13:01:51 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
-        System.out.println("hola tierra");
-        System.out.println("hola luna");
-        System.out.println("Hola Manola");
-
-        CLASE dato;
-        //HOLA DEVULETAAAAAA
+        System.out.println("estoy probando");
     }
 }
