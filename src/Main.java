0d2c774a1530ede69fd25aac79dbf5e662ec633c
LucasMartinCosta <lucassmartin.costa@gmail.com>
2024-05-24 10:30:51 -0300

    primer commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
-        System.out.println("hola mundo");
-        System.out.println("hola mundo");
-
-
+        System.out.println("hola tierra");
+        System.out.println("hola luna");
 
     }
 }
