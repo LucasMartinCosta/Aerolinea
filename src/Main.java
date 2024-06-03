895d1e3e7463aaa9674006ee06f86cdea35c3caf
ImanolSayago <imanolsayago0@gmail.com>
2024-06-03 18:50:09 -0300

    Agregue clase(manejo de archivos) y las librerias para poder trabajar con jackson

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner lector = new Scanner(System.in);
         Lista_vuelos listavuelos= new Lista_vuelos();
 
